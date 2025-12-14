f09cabd19122a2f1638457410ec917ebd195e4fa
sourav <souravkumar18835@gmail.com>
2025-12-15 00:19:35 +0530

    chore(layout): remove stars bg

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <BackgroundWrapper/>
+        {/* <BackgroundWrapper/> */}
         {children}
       </body>
     </html>
