b391e24dde90038b6ce4d8cf847922279dd0be8e
sourav <souravkumar18835@gmail.com>
2025-12-17 16:41:31 +0530

    add image config for lastfm music

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lastfm.freetls.fastly.net',
+        port: '',
+        pathname: '/i/u/**', 
+      },
+    ],
+  },
 };
 
 export default nextConfig;
